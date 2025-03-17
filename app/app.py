d8908c70bbd339e333e20130c0e00dc7f55a4eb8
Edvard Sørby <edvarso@stud.ntnu.no>
2025-03-17 10:01:20 -0700

    Add send_chat function

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from urllib.parse import urljoin
+import requests
 app = Flask(__name__)
 
 command_db = {
-  "command_1": "server_url_1",
+  "shrug": "http://localhost:5001",
   "command_2": "server_url_2",
   "command_3": "server_url_3"
 }
@@ -45,6 +47,13 @@ def chat():
           server_name = parts[1]
           command_db.pop(server_name)
           return "", 204
+      elif command in command_db:
+
+        return send_chat(command_db[command], message)
+
+      else:
+        return f"The command {command} is not registered.", 404
+
 
     chat_response = {
       "chat": f"{command}: {message}"
@@ -53,3 +62,23 @@ def chat():
     return jsonify(chat_response), 201
   else:
     return jsonify({'message': 'No data received'}), 400
+  
+
+def send_chat(server_url, chat):
+    full_url = urljoin(server_url, "/chat")
+
+    chat_request = {"chat": chat } 
+    
+    try: 
+        r = requests.post(full_url, json=chat_request)
+        r.raise_for_status()
+    except requests.exceptions.ConnectionError as err:
+        # eg, no internet
+        raise SystemExit(err)
+    except requests.exceptions.HTTPError as err:
+        # eg, url, server and other errors
+        raise SystemExit(err)
+
+    response_json = r.json()
+
+    return response_json
