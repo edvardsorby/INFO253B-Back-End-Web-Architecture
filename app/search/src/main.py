8dd90093e970abece361efec02a21dee81b35b9e
Vikramsingh Rathod <vikramsinghrathod@Vikramsinghs-MacBook-Pro.local>
2025-04-17 14:56:43 -0700

    add script to parse and upload

--- app/search/src/main.py
+++ app/search/src/main.py
@@ -5,4 +5,8 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    # Request could timeout
+    # base_dir = os.path.dirname(os.path.abspath(__file__))
+    # csv_path = os.path.join(base_dir, "bt_courses.csv")
+    # parse_csv_to_json.parse_csv_to_mongodb(csv_path, "mongodb+srv://rathodvikram44:rathodvikram44@allcourses.l4lqk7e.mongodb.net/", "allcourses", "allcourses")
     return {"message": "Hello World"}
