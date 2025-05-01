e909deae9d02f63ed3b784d1516ebcc014b23ae3
Edvard Sørby <edvarso@berkeley.edu>
2025-05-01 15:12:17 -0700

    Add ability to update only selected course fields

--- app/search/src/search/api/routes/courses.py
+++ app/search/src/search/api/routes/courses.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 
 from search.auth.dependencies import require_admin
 from search.db import Database
-from search.models import Course, SearchOut
+from search.models import Course, CourseUpdate, SearchOut
 from search.services import retrieve_courses
 from search.services.crud import add_course, delete_course, get_course, update_course
 
@@ -22,7 +22,7 @@ async def post(course: Course, db: Database, user=Depends(require_admin)):
     return await add_course(course, db)
 
 @router.put("/{id}")
-async def update(id: str, course: Course, db: Database, user=Depends(require_admin)):
+async def update(id: str, course: CourseUpdate, db: Database, user=Depends(require_admin)):
     return await update_course(id, course, db)
 
 @router.delete("/{id}")
